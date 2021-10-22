c6f2169bceba977d88ecacc5b413ad4e76f4017b
Aljaz Podgornik <aljaz.podgornik21@gmail.com>
2021-10-22 08:37:10 +0200

    Implemented database

---
+++ Unity/Stabilometry Analysis/Assets/Scripts/DatabaseScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System;
+
+public class DatabaseScript : MonoBehaviour
+{
+
+    private const string PatientTableName = "PatientTable",
+        MeasurementTableName = "MeasurementTable",
+        ParameterTableName = "ParametersTable",
+        DatabaseName = "database.db";
+
+    private SqliteConnection connection = null;
+
+    private void Awake()
+    {
+        OpenDatabase();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDatabase()
+    {
+        string connectionPath = "URI=file:" + Application.dataPath + @"/Database/" + DatabaseName;
+
+        try
+        {
+            connection = new SqliteConnection(connectionPath);
+            
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+
+        if (connection != null)
+        {
+            connection.Close();
+            Debug.Log("Done");
+        }
+    }
+
+    private void OnDestroy()
+    {
+            
+    }
+}
