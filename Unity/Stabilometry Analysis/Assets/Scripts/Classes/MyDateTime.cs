db76fe243de5128244539a6f7313a4210d6fee3b
Aljaz Podgornik <aljaz.podgornik21@gmail.com>
2022-02-08 23:54:22 +0100

    implemented MyDateTime

--- Unity/Stabilometry Analysis/Assets/Scripts/Classes/MyDateTime.cs
+++ Unity/Stabilometry Analysis/Assets/Scripts/Classes/MyDateTime.cs
@@ -4,5 +4,73 @@ using UnityEngine;
 
 public class MyDateTime
 {
-   
+    #region Variables
+
+    private const long hourMult = 100,
+        dayMult = 10000,
+        monthMult = 1000000,
+        yearMult = 100000000;
+
+    public int
+        year = 0,
+        month = 0,
+        day = 0,
+        hour = 0,
+        minutes = 0;
+
+    #endregion
+
+    public MyDateTime(int year, int month, int day, int hour, int minutes)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minutes = minutes;
+    }
+
+    /// <summary>
+    /// Takes in myDateTime format and fills the correct values.
+    /// </summary>
+    /// <param name="myDateTime"></param>
+    public MyDateTime(long myDateTime)
+    {
+        year = (int)(myDateTime / yearMult);
+        myDateTime -= year * yearMult;
+
+        month = (int)(myDateTime / monthMult);
+        myDateTime -= month * monthMult;
+
+        day = (int)(myDateTime / dayMult);
+        myDateTime -= day * dayMult;
+
+        hour = (int)(myDateTime / hourMult);
+
+        minutes = (int)(myDateTime - hour * hourMult);
+    }
+
+    /// <summary>
+    /// Returns the string version of the myDateFormat.
+    /// </summary>
+    /// <returns></returns>
+    override public string ToString()
+    {
+        return GetInt().ToString();
+    }
+
+    /// <summary>
+    /// Returns the date in format YYYYMMDDhhmm.
+    /// </summary>
+    /// <returns></returns>
+    public long GetInt()
+    {
+        long result = 0;
+        result += minutes;
+        result += hour * hourMult;
+        result += day * dayMult;
+        result += month * monthMult;
+        result += year * yearMult;
+
+        return result;
+    }
 }
--- Unity/Stabilometry Analysis/Assets/Scripts/TestScript.cs
+++ Unity/Stabilometry Analysis/Assets/Scripts/TestScript.cs
@@ -7,8 +7,11 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StabilometryTask test = new StabilometryTask();
-        test.Test();
+        //StabilometryTask test = new StabilometryTask();
+        //test.Test();
+        MyDateTime newDate = new MyDateTime(202212231854);
+        Debug.Log($"Year {newDate.year} Month {newDate.month} Day {newDate.day} Hour {newDate.hour} Minutes {newDate.minutes}");
+        Debug.Log($"{newDate.ToString()}");
     }
 
     // Update is called once per frame
