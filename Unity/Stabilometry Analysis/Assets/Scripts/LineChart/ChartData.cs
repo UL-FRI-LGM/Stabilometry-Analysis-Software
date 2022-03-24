5980e256e6b62d185dc7dced7464e57c3f5d336b
Aljaz Podgornik <aljaz.podgornik21@gmail.com>
2022-03-24 19:23:52 +0100

    started working on line chart

---
+++ Unity/Stabilometry Analysis/Assets/Scripts/LineChart/ChartData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChartData
+{
+    public float value;
+    public MyDateTime time;
+
+    public ChartData(float value, MyDateTime time)
+    {
+        this.value = value;
+        this.time = time;
+    }
+}
---
+++ Unity/Stabilometry Analysis/Assets/Scripts/LineChart/LineChartScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineChartScript : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private GameObject LineObject = null;
+
+    private RectTransform imageRect = null;
+    private List<ChartData> chartData = null;
+    #endregion
+
+    private void Awake()
+    {
+        this.imageRect = GetComponent<RectTransform>();
+    }
+
+    public void SetChartData(List<ChartData> chartData)
+    {
+        this.chartData = chartData;
+        UpdateChart();
+    }
+
+    /// <summary>
+    /// Draws the chart based on the current data and rect transform size.
+    /// </summary>
+    public void UpdateChart()
+    {
+        SetXLine(this.chartData);
+        SetYline(this.chartData);
+    }
+
+    private void SetXLine(List<ChartData> data)
+    {
+
+    }
+
+    private void SetYline(List<ChartData> data)
+    {
+
+    }
+}
