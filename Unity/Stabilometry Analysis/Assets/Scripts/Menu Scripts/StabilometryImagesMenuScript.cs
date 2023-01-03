44e41b44257e003f017b553577e45884c53665ce
Aljaz Podgornik <aljaz.podgornik21@gmail.com>
2023-01-03 23:37:36 +0100

    Implemented spawning of Image elements

--- Unity/Stabilometry Analysis/Assets/Scripts/Menu Scripts/StabilometryImagesMenuScript.cs
+++ Unity/Stabilometry Analysis/Assets/Scripts/Menu Scripts/StabilometryImagesMenuScript.cs
@@ -13,16 +13,29 @@ namespace StabilometryAnalysis
         [SerializeField] private AccordionRadioHandler poseRadioHandler = null;
         [SerializeField] private GameObject imageElementObjectHolder = null;
 
+        private RectTransform imageElementObjectRect = null;
+        private float prefabElementWidth = 0;
+
         private List<GameObject> imageElements = null;
         private List<StabilometryMeasurement> patientData = null;
         private List<StabilometryMeasurement> relevantData = null;
 
+        private int startDisplayNumber = 5;
+
+        private float previousScrollbarValue = 0;
+        private float imageElementObjectSize = 0;
+        private float initialElementObjectXPosition = 0;
+
+        private bool scrollbarSet = false;
         #endregion
 
         //public void 
         private void Awake()
         {
+            prefabElementWidth = stabilometryImageElementPrefab.GetComponent<RectTransform>().rect.width;
             imageElements = new List<GameObject>();
+            imageElementObjectRect = imageElementObjectHolder.GetComponent<RectTransform>();
+            initialElementObjectXPosition = imageElementObjectRect.localPosition.x;
         }
 
         private void OnEnable()
@@ -40,6 +53,25 @@ namespace StabilometryAnalysis
 
                 UpdateImages();
             }
+
+            if (scrollbarSet && scrollbarScript.valuePositon != previousScrollbarValue)
+                UpdatePosition(scrollbarScript.valuePositon, relevantData.Count);
+        }
+
+        private void GetPatientData()
+        {
+            patientData = mainScript.database.GetAllMeasurements(mainScript.currentPatient);
+            relevantData = GetRelevantData(patientData, poseRadioHandler.selectedPose);
+        }
+
+        private void UpdateImages()
+        {
+            int testNumber = 3;
+
+            SetScrollbar(testNumber);
+            scrollbarSet = true;
+
+            SpawnElements(testNumber);
         }
 
         private List<StabilometryMeasurement> GetRelevantData(List<StabilometryMeasurement> allData, Pose currentPose)
@@ -53,25 +85,49 @@ namespace StabilometryAnalysis
             return result;
         }
 
-        private void GetPatientData()
+        private void UpdatePosition(float newValue, float totalElementNumber)
         {
-            patientData = mainScript.database.GetAllMeasurements(mainScript.currentPatient);
-            relevantData = GetRelevantData(patientData, poseRadioHandler.selectedPose);
+            previousScrollbarValue = newValue;
+
+            imageElementObjectRect.localPosition =
+                new Vector3(
+                    ConvertToPosition(newValue, totalElementNumber, initialElementObjectXPosition),
+                    imageElementObjectRect.localPosition.y,
+                    imageElementObjectRect.localPosition.z);
         }
 
-        private void UpdateImages()
+        private float ConvertToPosition(float newValue, float totalElementNumber, float initialElementObjectXPosition)
         {
-            SetScrollbar(relevantData.Count);
-            SpawnElements();
+            // 0 mmeans at the left, 1 means right.
+            float maxChartPosition = initialElementObjectXPosition - (totalElementNumber - 1) * prefabElementWidth;
+
+            float result = newValue * (maxChartPosition - initialElementObjectXPosition) + initialElementObjectXPosition;
+            return result;
         }
 
-        private void SetScrollbar(int elemnetNumber)
+        private void SetScrollbar(int elementNumber)
         {
-
+            scrollbarScript.SetSize(elementNumber, startDisplayNumber);
         }
 
-        private void SpawnElements()
+        private void SpawnElements(int elementNumber)
         {
+
+            foreach (GameObject element in imageElements)
+            {
+                Destroy(element);
+            }
+
+            imageElements = new List<GameObject>();
+
+            for (int i = 0; i < elementNumber; i++)
+            {
+                GameObject instance = Instantiate(stabilometryImageElementPrefab, imageElementObjectHolder.transform);
+                RectTransform rect = instance.GetComponent<RectTransform>();
+                rect.anchoredPosition += new Vector2(i * rect.rect.width, 0);
+
+                imageElements.Add(instance);
+            }
 
         }
 
